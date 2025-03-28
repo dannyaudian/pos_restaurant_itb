9c78df45b7eed1913a0d080c5d95877aa0ad46c4
dannyaudian <danny.a.pratama@gmail.com>
2025-03-28 11:10:24 +0700

    Delete pos_restaurant_itb directory

--- pos_restaurant_itb/hooks.py
+++
@@ -1,8 +0,0 @@
-app_name = "pos_restaurant_itb"
-app_title = "POS Restaurant"
-app_publisher = "PT. Innovasi Terbaik Bangsa"
-app_description = "Restaurant POS Module"
-app_email = "info@inovasiterbaik.co.id"
-app_license = "MIT"
-
-fixtures = ["Custom Script", "Custom Field", "Property Setter", "Print Format", "Doctype", "Module Def"]
--- pos_restaurant_itb/public/js/waiter_ui.js
+++
@@ -1,9 +0,0 @@
-frappe.ui.form.on('KOT', {
-    refresh(frm) {
-        frm.add_custom_button('Send to Kitchen', () => {
-            frm.set_value('status', 'New');
-            frm.save();
-            frappe.msgprint('Order sent to kitchen!');
-        });
-    }
-});
