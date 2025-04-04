57bfe617c5e674d74c57360cacce1ef06658aad0
dannyaudian <danny.a.pratama@gmail.com>
2025-04-04 09:33:29 +0700

    Update hooks.py

--- pos_restaurant_itb/hooks.py
+++ pos_restaurant_itb/hooks.py
@@ -5,9 +5,6 @@ app_description = "Restaurant POS Module"
 app_email = "info@inovasiterbaik.co.id"
 app_license = "MIT"
 
-# Fixtures akan ditambahkan nanti setelah migrate berhasil
-
-
 fixtures = [
     "Client Script",
     "Server Script",
@@ -15,10 +12,7 @@ fixtures = [
     "Property Setter"
 ]
 
-
-# Tambahkan custom JS untuk Form bawaan ERPNext (POS Invoice, POS Profile, POS Order)
 doctype_js = {
-    "POS Order": "public/js/pos_order_autoname.js"
     "POS Order": "custom/pos_order/pos_order.js",
     "POS Invoice": "custom/pos_invoice/pos_invoice.js",
     "POS Profile": "custom/pos_profile/pos_profile.js"
