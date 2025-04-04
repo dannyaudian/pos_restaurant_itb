b472b60955bd2de69a63caaf9eeaec4f980950ea
dannyaudian <danny.a.pratama@gmail.com>
2025-04-04 06:46:06 +0000

    update setup py

---
+++ pos_restaurant_itb/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pos_restaurant_itb',
+    version='1.0.0',
+    description='POS Restaurant Management App for ERPNext',
+    author='PT. Innovasi Terbaik Bangsa',
+    author_email='support@itb.co.id',
+    packages=find_packages(),
+    zip_safe=False,
+    include_package_data=True,
+    install_requires=[],
+)
