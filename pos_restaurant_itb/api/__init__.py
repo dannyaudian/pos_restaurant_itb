9efd602ac5d1ba92c652306f916f40c9063ed23c
dannyaudian <danny.a.pratama@gmail.com>
2025-04-04 13:31:29 +0000

    test ganti init kot

--- pos_restaurant_itb/api/__init__.py
+++ pos_restaurant_itb/api/__init__.py
@@ -1,4 +1,4 @@
 from .load_pos_restaurant_config import load_pos_restaurant_config
 from .send_to_kitchen import send_to_kitchen
-from .kot_status_update import update_kot_status
+from .kot_status_update import update_kds_status_from_kot
 from .print import print_kot
