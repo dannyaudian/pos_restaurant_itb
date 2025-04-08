420bac6f2997bbd2b9c86d1fec7f74154cf9b1c6
dannyaudian <danny.a.pratama@gmail.com>
2025-04-08 10:43:22 +0700

    ubah config

--- pos_restaurant_itb/pos_restaurant_itb/config/desktop.py
+++ pos_restaurant_itb/pos_restaurant_itb/config/desktop.py
@@ -6,5 +6,32 @@ def get_data():
         "color": "#ff4d4f",
         "icon": "octicon octicon-clippy",
         "type": "module",
-        "label": _("Restaurant management")
+        "label": _("Restaurant Management"),
+        "items": [
+            {
+                "type": "doctype",
+                "name": "POS Order",
+                "label": _("POS Order"),
+                "description": _("Manage POS Orders")
+            },
+            {
+                "type": "doctype",
+                "name": "KOT",
+                "label": _("Kitchen Order Ticket (KOT)"),
+                "description": _("Manage Kitchen Order Tickets")
+            },
+            {
+                "type": "doctype",
+                "name": "Kitchen Display Order",
+                "label": _("Kitchen Display Order"),
+                "description": _("Manage Kitchen Display Orders")
+            },
+            {
+                "type": "page",
+                "name": "waiter",
+                "label": _("Waiter UI"),
+                "description": _("Interface for waiters to manage orders"),
+                "url": "/pos_restaurant_itb/waiter"
+            }
+        ]
     }]
