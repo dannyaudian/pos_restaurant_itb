6b869b915dcc387aac47a84421a9941f7a700b85
dannyaudian <danny.a.pratama@gmail.com>
2025-04-08 09:25:58 +0700

    Update create_kot.py

--- pos_restaurant_itb/api/create_kot.py
+++ pos_restaurant_itb/api/create_kot.py
@@ -31,6 +31,10 @@ def create_kot_from_pos_order(pos_order_id):
             pos_order = frappe.get_doc("POS Order", pos_order_id)
         except frappe.DoesNotExistError:
             frappe.throw(_("POS Order {0} tidak ditemukan.").format(pos_order_id))
+
+        # Validasi apakah POS Order sudah disave
+        if pos_order.docstatus == 0:
+            frappe.throw(_("POS Order belum disave. Silakan save terlebih dahulu sebelum mengirim item ke dapur."))
         
         # Validasi status dokumen
         validate_pos_order(pos_order)
