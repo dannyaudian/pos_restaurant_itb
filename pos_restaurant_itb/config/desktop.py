e8517701f3615b3a92a4c7eac8c90cfd8e367e0e
dannyaudian <danny.a.pratama@gmail.com>
2025-04-10 19:36:18 +0700

    testtt

--- pos_restaurant_itb/config/desktop.py
+++ pos_restaurant_itb/config/desktop.py
@@ -5,7 +5,7 @@ from frappe import _
 def get_data():
     return [
         {
-            "module_name": "POS Restaurant",
+            "module_name": "POS Restaurant ITB",
             "color": "blue",
             "icon": "octicon octicon-file-directory",
             "type": "module",
